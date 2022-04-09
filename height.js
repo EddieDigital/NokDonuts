e7be44218ae077c8ce670af773371274e327db89
EddieDigital <102945940+EddieDigital@users.noreply.github.com>
2022-04-08 18:54:07 -0700

    Add files via upload

---
+++ height.js
@@ -0,0 +1,12 @@
+let heighter = $(".donutMaker").height();
+$(".headingPicture").on("click", function(){
+  $(this).height(heighter);
+});
+/*
+let box = $('#donutPicture');
+let heighter = box.height();
+if(heighter < 910){
+$("#donutPicture").css("height", "heighter")
+}
+alert("aming")
+*/
